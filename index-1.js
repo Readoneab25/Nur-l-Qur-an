0b052e500f0f080ea0e2376b1a62a57a93848245
Readoneab25 <ridwanullahi103@gmail.com>
2026-03-24 00:59:56 +0100

    Add files via upload

---
+++ index-1.js
@@ -0,0 +1,34 @@
+
+document.getElementById("tog").onclick = function(){
+    document.getElementById("cont").style.margin = "0 50%"
+    document.getElementById("tog").style.display = "none"
+    document.getElementById("to").style.display = "flex"
+}
+document.getElementById("to").onclick = function(){
+    document.getElementById("cont").style.margin = "0 100%"
+    document.getElementById("tog").style.display = "flex"
+    document.getElementById("to").style.display = "none"
+}
+document.getElementById("s1").onclick = function(){
+    document.getElementById("cont").style.margin = "0 100%"
+    document.getElementById("tog").style.display = "flex"
+    document.getElementById("to").style.display = "none"
+
+}
+document.getElementById("s2").onclick = function(){
+    document.getElementById("cont").style.margin = "0 100%"
+    document.getElementById("tog").style.display = "flex"
+    document.getElementById("to").style.display = "none"
+}
+document.getElementById("contact").onclick = function(){
+    window.location.href = "https://wa.me/+2349138907913"
+}
+document.getElementById("sur").onclick = function(){
+    window.location.href = "text.html"
+}
+document.getElementById("re").onclick = function(){
+    window.location.href = "resat.html"
+}
+document.getElementById("na").onclick = function(){
+    window.location.href = "name.html"
+}
